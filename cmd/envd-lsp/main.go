383686be268216e9cfd4023b1c68879c8286a39f
Ce Gao <ce.gao@outlook.com>
2022-06-20 22:03:00 +0800

    feat: Init
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

---
+++ cmd/envd-lsp/main.go
@@ -0,0 +1,105 @@
+// Copyright 2022 The envd Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tensorchord/envd/pkg/version"
+	cli "github.com/urfave/cli/v2"
+	"go.lsp.dev/protocol"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/tensorchord/envd-lsp/pkg/lsp"
+)
+
+// go lsp server uses zap, thus we have to keep two loggers (logrus and zap) in envd.
+// zap is only used in lsp subcommand.
+var logLevel = zap.NewAtomicLevelAt(zapcore.WarnLevel)
+
+func startLSP(clicontext *cli.Context) error {
+	logger, cleanup := newzapLogger()
+	defer cleanup()
+	ctx := protocol.WithLogger(clicontext.Context, logger)
+
+	s := lsp.New()
+	err := s.Start(ctx, clicontext.String("address"))
+	return err
+}
+
+func newzapLogger() (logger *zap.Logger, cleanup func()) {
+	cfg := zap.NewDevelopmentConfig()
+	cfg.Level = logLevel
+	cfg.Development = false
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(fmt.Errorf("failed to initialize logger: %v", err))
+	}
+
+	cleanup = func() {
+		_ = logger.Sync()
+	}
+	return logger, cleanup
+}
+
+func main() {
+	cli.VersionPrinter = func(c *cli.Context) {
+		fmt.Println(c.App.Name, version.Package, version.GetVersion().String())
+	}
+
+	app := cli.NewApp()
+	app.Name = "envd-lsp"
+	app.Usage = "language server for envd"
+	app.Version = version.GetVersion().String()
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "address",
+			Usage: "Address (hostname:port) to listen on",
+		},
+		&cli.BoolFlag{
+			Name:  "debug",
+			Usage: "Enable debug logging",
+		},
+	}
+
+	// Deal with debug flag.
+	var debugEnabled bool
+
+	app.Before = func(context *cli.Context) error {
+		debugEnabled = context.Bool("debug")
+
+		if debugEnabled {
+			logLevel.SetLevel(zapcore.DebugLevel)
+		}
+		return nil
+	}
+
+	app.Action = startLSP
+	handleErr(debugEnabled, app.Run(os.Args))
+}
+
+func handleErr(debug bool, err error) {
+	if err == nil {
+		return
+	}
+	if debug {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	} else {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	}
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,35 @@
+module github.com/tensorchord/envd-lsp
+
+go 1.18
+
+require (
+	github.com/cockroachdb/errors v1.9.0
+	github.com/tensorchord/envd v0.1.1-0.20220620134703-6bd547ea3f31
+	github.com/tilt-dev/starlark-lsp v0.0.0-20220517144608-76df2b44bc8a
+	github.com/urfave/cli/v2 v2.8.1
+	go.lsp.dev/jsonrpc2 v0.10.0
+	go.lsp.dev/protocol v0.12.0
+	go.uber.org/zap v1.21.0
+)
+
+require (
+	github.com/cockroachdb/logtags v0.0.0-20211118104740-dabe8e521a4f // indirect
+	github.com/cockroachdb/redact v1.1.3 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/getsentry/sentry-go v0.12.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/segmentio/asm v1.1.3 // indirect
+	github.com/segmentio/encoding v0.3.4 // indirect
+	github.com/smacker/go-tree-sitter v0.0.0-20220209044044-0d3022e933c3 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	go.lsp.dev/pkg v0.0.0-20210717090340-384b27a52fb2 // indirect
+	go.lsp.dev/uri v0.3.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
